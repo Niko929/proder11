4c4b338c12c1f45f8aef9a1c05d4802e483e1ceb
MSI <ekaterinanikolskaa2@gmail.com>
2025-02-24 12:59:19 +0400

    homework_10_1

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,8 @@
-from typing import List, Dict
-
-"""Функция фильтрует данные ключ которых совпадаетс аргументом state"""
+from typing import Any, Dict, List
 
 
-def filter_by_state(rey_fultr: List[Dict[str,int]], state="EXECUTED") -> list[dict[str,int]]:
+def filter_by_state(rey_fultr: List[Dict[str, Any]], state: str = "EXECUTED") -> list[dict[str, Any]]:
+    """Функция фильтрует данные ключ которых совпадают по значению с параметром state"""
     # ter = []
     #  for req in rey_fultr:
     # if req["state"] == state:
@@ -11,9 +10,7 @@ def filter_by_state(rey_fultr: List[Dict[str,int]], state="EXECUTED") -> list[di
     return [req for req in rey_fultr if req.get("state") == state]
 
 
-""" Функция сортирует данные по дате, от больше к меньшему"""
-
-
 def sort_by_date(sort_dat: list[dict], reverse: bool = True) -> list[dict]:
+    """Функция сортирует данные по дате, от большего к меньшему"""
     hg = sorted(sort_dat, key=lambda x: x["date"], reverse=reverse)
     return hg
