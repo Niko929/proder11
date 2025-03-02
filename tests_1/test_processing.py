a94673053ac0312c4604888be874424e4ef836fb
MSI <ekaterinanikolskaa2@gmail.com>
2025-03-02 11:15:43 +0400

    homework_10_2 NEW1

--- tests_1/test_processing.py
+++ tests_1/test_processing.py
@@ -1,9 +1,6 @@
 from typing import Any, Dict, List
-
 import pytest
-
 from src.processing import filter_by_state, sort_by_date
-
 
 @pytest.mark.parametrize(
     "h , h1",
@@ -33,24 +30,6 @@ from src.processing import filter_by_state, sort_by_date
 )
 def test_filter_by_state(h, h1):
     assert filter_by_state(h) == h1
-
-
-def test_filter_by():
-    assert filter_by_state(
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-        ],
-        "EXECUTED",
-    ) == (
-        [
-            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        ]
-    )
-
 
 @pytest.mark.parametrize(
     "g , g1",
--- tests_1/test_widget.py
+++ tests_1/test_widget.py
@@ -3,11 +3,12 @@ import pytest
 from src.widget import get_date, mask_account_card
 
 
-def test_get_date():
-    assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
-    assert get_date("2014-03-11T02:26:18.671407") == "11.03.2014"
-    with pytest.raises(ValueError):
-        get_date("")
+@pytest.mark.parametrize("date_1 , date_2",[
+    ("2024-03-11T02:26:18.671407" , "11.03.2024"),
+    ("2025-03-11T02:26:18.671407" , "11.03.2025"),
+])
+def test_get_date(date_1 , date_2):
+    assert get_date(date_1) == date_2
 
 
 @pytest.mark.parametrize(
