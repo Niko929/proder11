2ff9b4202539230d371e60d75bdbf38c9095c703
MSI <ekaterinanikolskaa2@gmail.com>
2025-03-18 20:44:49 +0400

    homework_11_2 nn

--- tests_1/test_decorat.py
+++ tests_1/test_decorat.py
@@ -1,20 +1,45 @@
 import pytest
 from src.decorators import log
-from src.test import my_function
+from typing import Any
 
 
-def test_log(Any):
-    @log(function="my.txt")
+
+def test_log():
+    @log()
     def my_function(x, y):
         return x + y
-
     result = my_function(1, 5)
     assert result == 6
 
 
-@pytest.mark.parametrize('fr,fr2',[
-    (1,2),('my_function ok'),
-])
+def test_success_case_in_file():
+    args = (1, "2")
 
-def test_dever(fr,fr2):
-    assert log(fr) == fr2
+    @log(filename="test.txt")
+    def some_func(x, y):
+        return x + y
+
+    some_func(*args)
+    with open("test.txt", "r", encoding="utf-8") as f:
+        assert f.readlines()[-1] == f"{some_func.__name__} error: {TypeError}. Inputs:{args}"
+
+def test_success_case_in_console(capsys):
+    args = (1, "2")
+
+    @log()
+    def some_func(x, y):
+        return x + y
+
+    some_func(*args)
+    assert capsys.readouterr().out == f"{some_func.__name__} error: {TypeError}. Inputs:{args}\n"
+
+
+def test_success_case(capsys):
+    args = (1, 2)
+
+    @log()
+    def some_fun(x, y):
+        return x + y
+
+    some_fun(*args)
+    assert capsys.readouterr().out == f"some_fun ok\n"
