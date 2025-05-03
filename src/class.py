dce8b860737cb2d357f6d903d845111aa6a65724
MSI <ekaterinanikolskaa2@gmail.com>
2025-05-03 12:39:55 +0400

    hom_work 14/1 ново

--- src/class.py
+++ src/class.py
@@ -1,3 +1,4 @@
+
 class Product:
     def __init__(self, name: str, price: float, quantity: int):
         self.name = name
@@ -33,6 +34,8 @@ class Product:
 
 
 
+
+
 class Category:
     total_categories = 0
     total_products = 0
@@ -40,7 +43,10 @@ class Category:
     def __init__(self, name: str, description: str):
         self.name = name
         self.description = description
-        self.__products = []  # Приватный атрибут для хранения списка товаров
+        self.products = []  # Приватный атрибут для хранения списка товаров
+
+
+
 
         Category.total_categories += 1
 
@@ -64,6 +70,48 @@ class Category:
 
     def __str__(self):
         return f"{self.name}, количество продуктов:{self.description} "
+
+
+
+class Smartphone(Product):
+    efficiency: str
+    model: str
+    memory: int
+    color: str
+
+    def __init__(self, name: str, price: float, quantity: int):
+        super().__init__(name, price, quantity)
+        self.products = []
+
+    def __add__(self, other):
+        if not isinstance(other, Product):
+            raise TypeError("Можно складывать только объекты одного класса Product.")
+
+        if self.__class__ is not other.__class__:
+            raise TypeError("Нельзя складывать продукты разных типов.")
+
+        total_price = (self.price * self.quantity) + (other.price * other.quantity)
+        total_quantity = self.quantity + other.quantity
+        # Создаем новый объект Product с суммарной стоимостью и количеством
+        return Product("Суммарный товар", total_price / total_quantity if total_quantity > 0 else 0, total_quantity)
+
+    def add_product(self, product):
+        if not isinstance(product, Product):
+            raise TypeError("Можно добавлять только объекты класса Product или его наследников.")
+
+        self.products.append(product)
+
+
+
+
+
+
+
+class LawnGrass(Product):
+    country: str
+    germination_period: int
+    color: str
+
 
 
 product_data = {
